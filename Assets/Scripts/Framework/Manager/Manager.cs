922af1287c48472d841e954166cb6e97f24ff731
lengyueling <1030952490@qq.com>
2022-12-17 23:06:58 +0800

    add project

---
+++ Assets/Scripts/Framework/Manager/Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 作为所有manager的入口
+/// </summary>
+public class Manager : MonoBehaviour
+{
+    private static ResourceManager _resource;
+    public static ResourceManager Resource
+    {
+        get { return _resource; }
+    }
+
+    private void Awake()
+    {
+        _resource = this.gameObject.AddComponent<ResourceManager>();
+    }
+}
